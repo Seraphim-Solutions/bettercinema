c35c15dea8d9a9ea0ede76be968efc21149eadc4
DanniSec <agarczechsparta@gmail.com>
2022-08-19 07:47:24 +0100

    added custom color support

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from rich.traceback import install
 from rich.table import Table
 import xml.etree.ElementTree as ET
 import hashlib
+import json
 
 from api.api import BetterCinemaAPI
 from handlers.request_parser import Handler
@@ -30,6 +31,15 @@ class Cli():
         self.movie_negative_votes = []
         self.movie_links = []
         self.page = 0
+        # load config/config.json
+        with open('config/config.json') as config_file:
+            self.config = json.load(config_file)
+        self.color_neutral = self.config['colors']['neutral']
+        self.color_good = self.config['colors']['good']
+        self.color_bad = self.config['colors']['bad']
+        self.color_warning = self.config['colors']['warning']
+        self.color_info = self.config['colors']['info']
+        self.color_primary = self.config['colors']['primary']
 
     def login(self):
         if self.db.read_creds() == []:
@@ -118,17 +128,17 @@ class Cli():
             self.movie_postive_votes.append(movie[3])
             self.movie_negative_votes.append(movie[4])
         
-        self.movie_table = Table(show_header=True, header_style="bold white", title="Search Results", title_justify="centre")
-        self.movie_table.add_column("#", style="bold blue", justify="middle")
-        self.movie_table.add_column("Name", style="bold green", justify="middle")
-        self.movie_table.add_column("Size", style="bold yellow", justify="middle")
+        self.movie_table = Table(show_header=True, header_style=self.color_neutral, title="Search Results", title_justify="centre")
+        self.movie_table.add_column("#", style=self.color_primary, justify="middle")
+        self.movie_table.add_column("Name", style=self.color_good, justify="middle")
+        self.movie_table.add_column("Size", style=self.color_warning, justify="middle")
         self.movie_table.add_column("Votes", justify="middle", no_wrap=True)
         for i in range(len(self.movie_names)):
-            self.movie_table.add_row(f"[bold blue]{str(i + 1)}[/]",
+            self.movie_table.add_row(f"[{self.color_primary}]{str(i + 1)}[/]",
             self.movie_names[i],
-            f"[bold yellow]{self.movie_sizes[i]}[/]",
-            (f"[bold green]{self.movie_postive_votes[i]}[/] [bold white]|[/] [bold red]{self.movie_negative_votes[i]}[/]"),
-            style=("bold white" if self.movie_postive_votes[i] == self.movie_negative_votes[i] else ("bold" if self.movie_postive_votes[i] >= self.movie_negative_votes[i] else "bold red")))
+            f"[{self.color_warning}]{self.movie_sizes[i]}[/]",
+            (f"[{self.color_good}]{self.movie_postive_votes[i]}[/] [{self.color_neutral}]|[/] [{self.color_bad}]{self.movie_negative_votes[i]}[/]"),
+            style=(self.color_neutral if self.movie_postive_votes[i] == self.movie_negative_votes[i] else ("bold" if self.movie_postive_votes[i] >= self.movie_negative_votes[i] else self.color_bad)))
 
         console.print(self.movie_table)
         self.select_item_from_results()
