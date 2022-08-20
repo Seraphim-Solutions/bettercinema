a5b1cc603839889fe5466577f782072bcb7c7b27
TrivariAlthea <jakub.velimsky@tuta.io>
2022-08-20 17:24:02 +0200

    Automation

---
+++ builds/linux/handlers/request_parser.py
@@ -0,0 +1,34 @@
+import requests
+import urllib
+import os 
+
+class Handler():
+    def __init__(self):
+        self.base_url = 'https://webshare.cz/'
+
+    def login(self, username, password_hash):
+        url = "https://webshare.cz/api/login/"
+
+        payload={'username_or_email': username,
+        'password': password_hash,
+        'keep_logged_in': '1'}
+        headers = {'X-Requested-With':'XMLHttpRequest','Accept':'text/xml; charset=UTF-8','Referer':self.base_url}
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        return response.content
+
+    def salt(self, username):
+        url = "https://webshare.cz/api/salt/"
+
+        payload={'username_or_email': username}
+        headers = {'X-Requested-With':'XMLHttpRequest','Accept':'text/xml; charset=UTF-8','Referer':self.base_url}
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        return response.content
+    
+    def download(self, filename, url):
+        if not os.path.exists('downloads'):
+            os.makedirs('downloads')
+        urllib.request.urlretrieve(url, f'downloads/{filename}')
---
+++ builds/windows/handlers/infuse_handler.py
@@ -0,0 +1,12 @@
+import os
+import platform
+
+class Player_Infuse():
+    def __init__(self):
+        pass
+        
+    def play(self, url):
+        if platform.system() == 'Darwin':
+            os.system(f'open "infuse://x-callback-url/play?url={url}"')
+        else:
+            print('Not supported')
