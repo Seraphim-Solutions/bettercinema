38ea79ec37e0f7015419f6e6825f70beb299315c
Jakub Vel <34968650+trivarialthea@users.noreply.github.com>
2022-08-20 12:59:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ class Cli():
         self.bc = BetterCinemaAPI()
         self.rp = Handler()
         self.player = Player()
-        self.player = Player_Infuse()
+        self.player_infuse = Player_Infuse()
         self.db = db()
         self.movie_names = []
         self.movie_idents = []
