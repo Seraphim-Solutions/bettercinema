11c06591d36ac310c14baeb261c6feffa30fafe0
DanniSec <agarczechsparta@gmail.com>
2022-09-14 08:45:01 +0200

    Fixed 'None' if binary for system didn't exist

--- handlers/version_handler.py
+++ handlers/version_handler.py
@@ -24,4 +24,7 @@ class version_handler:
     def get_latest_version(self):
         for assets in self.response['assets']:
             if re.match(f"BetterCinema_{platform.system()}*", assets['name']):
-                return f"Direct link: {assets['browser_download_url']}"    
+                return f"Direct link: {assets['browser_download_url']}"
+            else:
+                return f"Sorry, but binary file for {platform.system()} has not been released yet.\nFor more information check latest release: https://github.com/Seraphim-Solutions/bettercinema/releases/tag/v1.1.1"
+
