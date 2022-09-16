ce67d8e0c1d4811e7734584c1c2d3dc7ecb098e3
DanniSec <agarczechsparta@gmail.com>
2022-09-16 10:47:09 +0100

    code cleaning + spinner

--- handlers/trakt/search.py
+++ handlers/trakt/search.py
@@ -1,17 +1,26 @@
+"""Import modules"""
 import json
 import requests
 
+from rich.console import Console
+
+
 class Search:
+    """Handle trakt search"""
     def __init__(self):
-        with open('./config/trakt_config.json') as f:
-            self.trakt_config = json.load(f)
+        with open('./config/trakt_config.json', encoding="utf-8") as config:
+            self.trakt_config = json.load(config)
+        self.console = Console()
 
-    def get(self, query, type):
-        headers = {
-        'Content-Type': 'application/json',
-        'trakt-api-version': '2',
-        'trakt-api-key': self.trakt_config['application_auth']['client_id']
-        }
+    def get(self, query, search_type):
+        """Handle search request"""
+        with self.console.status(f"Searching for {query}", spinner="dots"):
+            headers = {
+            'Content-Type': 'application/json',
+            'trakt-api-version': '2',
+            'trakt-api-key': self.trakt_config['application_auth']['client_id']
+            }
 
-        request = requests.get(f'https://api.trakt.tv/search/{type}?query={query}', headers=headers)
-        return request.json()
+            request = requests.get(f'https://api.trakt.tv/search/{search_type}?query={query}',
+            headers=headers, timeout=30)
+            return request.json()
